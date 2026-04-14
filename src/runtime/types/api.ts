7e192466befab9e7010891ea1340ed3870cdb5e1
Evan Schleret <evan@schleret.ch>
2026-04-15 00:17:53 +0200

    fix(types): remove explicit any in submission response

--- src/runtime/types/api.ts
+++ src/runtime/types/api.ts
@@ -73,8 +73,8 @@ export interface FormForgeSubmitInput {
 }
 
 export interface FormForgeSubmissionResponse {
-  data: Record<string, any>
-  meta?: Record<string, any>
+  data: FormForgeJsonObject
+  meta?: FormForgeJsonObject
 }
 
 export interface FormForgeResponsesListResponse {
